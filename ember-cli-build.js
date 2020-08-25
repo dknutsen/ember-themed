0428c63b32b72c02b680dd6e3280ee35ba316c4a
Daniel Knutsen <dknutsen@gmail.com>
2020-08-24 22:01:25 -0500

    Add tailwind to dummy app

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,15 +4,19 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
-    // Add options here
+    postcssOptions: {
+      compile: {
+        plugins: [
+          {
+            module: require('postcss-import'),
+            options: {
+              path: ['node_modules']
+            }
+          },
+          require('tailwindcss')('./tests/dummy/app/tailwind/config.js')
+        ]
+      }
+    }
   });
-
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
-
   return app.toTree();
 };
