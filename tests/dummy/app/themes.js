406087470552b9a427ec93417a37d6b74ecf097f
Daniel Knutsen <dknutsen@gmail.com>
2020-08-26 21:41:21 -0500

    Extract transition into interpolatable var in dummy themes

--- tests/dummy/app/themes.js
+++ tests/dummy/app/themes.js
@@ -1,11 +1,13 @@
+const transition = 'transition-colors duration-500 ease-in-out';
+
 const themes = {
   light: {
-    default: 'bg-white text-black transition duration-500 ease-in-out',
-    primary: 'bg-blue-500 text-white transition duration-500 ease-in-out',
+    default: `bg-white text-black ${transition}`,
+    primary: `bg-blue-500 text-white ${transition}`,
   },
   dark: {
-    default: 'bg-gray-900 text-white transition duration-500 ease-in-out',
-    primary: 'bg-blue-600 text-white transition duration-500 ease-in-out',
+    default: `bg-gray-900 text-white ${transition}`,
+    primary: `bg-blue-600 text-white ${transition}`,
   }
 };
 
@@ -18,8 +20,8 @@ const variants = {
 }
 Object.keys(variants).forEach(variant => {
   const color = variants[variant];
-  themes.light[`alert-${variant}`] = `bg-${color}-200 text-${color}-800 border border-${color}-500`;
-  themes.dark[`alert-${variant}`] = `bg-${color}-800 text-${color}-200 border border-${color}-500`;
+  themes.light[`alert-${variant}`] = `bg-${color}-200 text-${color}-800 border border-${color}-500 ${transition}`;
+  themes.dark[`alert-${variant}`] = `bg-${color}-800 text-${color}-200 border border-${color}-500 ${transition}`;
 });
 
 export default themes;
