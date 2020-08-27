fbccf78c88fb5844484d49efeb39081c67830ffc
Daniel Knutsen <dknutsen@gmail.com>
2020-08-26 21:11:19 -0500

    Add more dummy components and start expanding dummy themes a bit

--- tests/dummy/app/themes.js
+++ tests/dummy/app/themes.js
@@ -1,10 +1,25 @@
-export default {
+const themes = {
   light: {
-    default: 'light',
-    primary: 'light-primary',
+    default: 'bg-white text-black transition duration-500 ease-in-out',
+    primary: 'bg-blue-500 text-white transition duration-500 ease-in-out',
   },
   dark: {
-    default: 'dark',
-    primary: 'dark-primary',
+    default: 'bg-gray-900 text-white transition duration-500 ease-in-out',
+    primary: 'bg-blue-600 text-white transition duration-500 ease-in-out',
   }
 };
+
+const variants = {
+  primary: 'blue',
+  success: 'green',
+  warning: 'yellow',
+  danger: 'red',
+  default: 'gray'
+}
+Object.keys(variants).forEach(variant => {
+  const color = variants[variant];
+  themes.light[`alert-${variant}`] = `bg-${color}-200 text-${color}-800 border border-${color}-500`;
+  themes.dark[`alert-${variant}`] = `bg-${color}-400 text-${color}-900 border border-${color}-700`;
+});
+
+export default themes;
