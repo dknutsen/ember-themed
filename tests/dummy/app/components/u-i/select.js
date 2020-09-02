854a9fb8fc0ec15cbdaad8781f00e15402806a9e
Daniel Knutsen <dknutsen@gmail.com>
2020-09-01 21:55:29 -0500

    Convert select to template only

--- tests/dummy/app/components/u-i/select.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class SelectComponent extends Component {
-  @action onChange(event) {
-    if (this.args.onChange) this.args.onChange(event.target.value);
-  }
-}
