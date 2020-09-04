e0ee00004c48b1390c54d66b73774a9b15678245
Daniel Knutsen <dknutsen@gmail.com>
2020-09-03 21:29:38 -0500

    Changes to navbar and application template

--- tests/dummy/app/services/ember-themed.js
+++ tests/dummy/app/services/ember-themed.js
@@ -3,7 +3,7 @@ import themes from 'dummy/themes';
 
 export default class ThemeService extends EmberThemedService {
   // this is the default theme to fall back to if one is not yet set via `setTheme()`
-  defaultTheme = 'light';
+  defaultTheme = 'dark';
 
   themes = themes;
 }
