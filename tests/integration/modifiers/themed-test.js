09833f22dba831a059026a063367078267f1bef3
Daniel Knutsen <dknutsen@gmail.com>
2020-09-08 20:09:54 -0500

    Update modifier test with one very basic test and service stub

--- tests/integration/modifiers/themed-test.js
+++ tests/integration/modifiers/themed-test.js
@@ -2,14 +2,28 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import ThemedService from 'ember-themed/services/ember-themed';
+
+class ThemedStub extends ThemedService {
+  defaultTheme = 'light';
+  themes = {
+    light: {
+      default: 'something else',
+      branded: 'brand-color brand-text'
+    }
+  };
+}
 
 module('Integration | Modifier | themed', function(hooks) {
   setupRenderingTest(hooks);
 
-  // Replace this with your real tests.
-  test('it renders', async function(assert) {
-    await render(hbs`<div {{themed}}></div>`);
+  hooks.beforeEach(function(assert) {
+    this.owner.register('service:ember-themed', ThemedStub);
+  });
 
-    assert.ok(true);
+  test('it applies the current theme default style when no arguments are provided', async function(assert) {
+    await render(hbs`<div {{themed}}></div>`);
+    assert.dom('div').hasClass('something');
+    assert.dom('div').hasClass('else');
   });
 });
