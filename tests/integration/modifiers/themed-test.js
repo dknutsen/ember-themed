821ec0ca8956073d62b5afcc7388066be193a5f9
Daniel Knutsen <dknutsen@gmail.com>
2020-09-09 20:53:20 -0500

    Add more tests for themed modifier

--- tests/integration/modifiers/themed-test.js
+++ tests/integration/modifiers/themed-test.js
@@ -1,17 +1,46 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, settled } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import ThemedService from 'ember-themed/services/ember-themed';
 
 class ThemedStub extends ThemedService {
   defaultTheme = 'light';
+  matchSystemTheme = false;
   themes = {
     light: {
-      default: 'something else',
-      branded: 'brand-color brand-text'
+      default: 'something light',
+      branded: 'light-brand-color light-brand-text',
+      'card-red': 'header-light-red border-light-red',
+      'card-blue': 'header-light-blue border-light-blue',
+    },
+    dark: {
+      default: 'something-else dark',
+      branded: 'dark-brand-color dark-brand-text',
+      'card-red': 'header-dark-red border-dark-red',
+      'card-blue': 'header-dark-blue border-dark-blue',
     }
   };
+}
+
+// asserts the the given element only has the classes for the given theme/style/variant combo, and nothing more
+const onlyHasStyle = function(assert, element, service, theme, style, variant = '') {
+  const fullStyleName = [style, variant].filter(a=>a).join('-');
+  const styleClasses = service.getThemeStyle(theme, fullStyleName);
+  styleClasses.forEach(classToHave => {
+    assert.dom(element).hasClass(classToHave);
+  });
+  const notStyleClasses = service.themeNames.reduce((notClasses, themeName) => {
+    Object.keys(service.themes[themeName]).forEach(styleName => {
+      if (theme !== themeName || fullStyleName !== styleName) {
+        notClasses = `${notClasses} ${service.getThemeStyle(themeName, styleName).join(' ')}`;
+      }
+    });
+    return notClasses;
+  }, '');
+  notStyleClasses.split(' ').forEach(classNotToHave => {
+    assert.dom(element).doesNotHaveClass(classNotToHave);
+  });
 }
 
 module('Integration | Modifier | themed', function(hooks) {
@@ -22,8 +51,58 @@ module('Integration | Modifier | themed', function(hooks) {
   });
 
   test('it applies the current theme default style when no arguments are provided', async function(assert) {
-    await render(hbs`<div {{themed}}></div>`);
-    assert.dom('div').hasClass('something');
-    assert.dom('div').hasClass('else');
+    await render(hbs`<div id="modified" {{themed}}></div>`);
+    this.emberThemed = this.owner.lookup('service:ember-themed');
+
+    // assert applying style works as expected
+    onlyHasStyle(assert, '#modified', this.emberThemed, 'light', 'default');
+
+    this.emberThemed.setTheme('dark');
+    await settled();
+
+    // assert applying default style from new theme works as expected
+    onlyHasStyle(assert, '#modified', this.emberThemed, 'dark', 'default');
+  });
+
+  test('it applies the specified style when a style is provided/changed', async function(assert) {
+    this.set('currentStyle', 'branded');
+    await render(hbs`<div id="modified" {{themed this.currentStyle}}></div>`);
+    this.emberThemed = this.owner.lookup('service:ember-themed');
+
+    // assert applying style works as expected
+    onlyHasStyle(assert, '#modified', this.emberThemed, 'light', 'branded');
+
+    this.emberThemed.setTheme('dark');
+    await settled();
+
+    // assert changing theme changes to the specific style in the new theme
+    onlyHasStyle(assert, '#modified', this.emberThemed, 'dark', 'branded');
+
+    this.set('currentStyle', 'card-blue');
+    await settled();
+
+    // assert changing style changes to the new style in the current theme
+    onlyHasStyle(assert, '#modified', this.emberThemed, 'dark', 'card-blue');
+  });
+
+  test('it applies the specified style when a style and variant are provided', async function(assert) {
+    this.set('currentVariant', 'red');
+    await render(hbs`<div id="modified" {{themed "card" this.currentVariant}}></div>`);
+    this.emberThemed = this.owner.lookup('service:ember-themed');
+
+    // assert applying style with variant works as expected
+    onlyHasStyle(assert, '#modified', this.emberThemed, 'light', 'card', 'red');
+
+    this.emberThemed.setTheme('dark');
+    await settled();
+
+    // assert changing theme changes to the specific style/variant in the new theme
+    onlyHasStyle(assert, '#modified', this.emberThemed, 'dark', 'card', 'red');
+
+    this.set('currentVariant', 'blue');
+    await settled();
+
+    // assert changing variant changes to the new style/variant in the current theme
+    onlyHasStyle(assert, '#modified', this.emberThemed, 'dark', 'card-blue');
   });
 });
