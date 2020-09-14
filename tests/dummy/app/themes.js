7f42be587035a8dda0a72e7c062a7275c5ed3f07
Daniel Knutsen <dknutsen@gmail.com>
2020-09-14 16:48:09 -0500

    Add demo component and a hero to the index page

--- tests/dummy/app/themes.js
+++ tests/dummy/app/themes.js
@@ -14,11 +14,13 @@ const themes = {
   light: {
     default: `bg-white text-black ${transition}`,
     code: `bg-gray-200 text-black ${transition}`,
+    demo: `border-gray-600 ${transition}`,
     ...generateVariants('callout', variants, (color) => `bg-${color}-200 text-${color}-800 border-${color}-500 ${transition}`)
   },
   dark: {
     default: `bg-gray-900 text-white ${transition}`,
     code: `bg-gray-800 text-white ${transition}`,
+    demo: `border-gray-400 ${transition}`,
     ...generateVariants('callout', variants, (color) => `bg-${color}-800 text-${color}-200 border-${color}-500 ${transition}`)
   }
 };
