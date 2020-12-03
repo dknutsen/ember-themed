89e5a97c01345a83c0adb4694f4451f117c9a0e8
Daniel Knutsen <dknutsen@gmail.com>
2020-12-02 21:47:18 -0600

    Make sure dummy app themes object is tracked for experiments

--- tests/dummy/app/services/ember-themed.js
+++ tests/dummy/app/services/ember-themed.js
@@ -1,5 +1,10 @@
 import EmberThemedService from 'ember-themed/services/ember-themed';
 import themes, { variants } from 'dummy/themes';
+import { TrackedObject } from 'tracked-built-ins';
+
+// make sure our themes object is tracked since we'll be doing some crazy stuff with it
+// in the dummy app
+const trackedThemes = new TrackedObject(themes);
 
 export default class ThemeService extends EmberThemedService {
   // this is the default theme to fall back to if one is not yet set via `setTheme()`
@@ -13,7 +18,7 @@ export default class ThemeService extends EmberThemedService {
 
   // Set your themes on the service, you could also define them here instead of in the
   // themes.js file if you choose
-  themes = themes;
+  themes = trackedThemes;
 
   // misc stuff for the dummy app
   variantNames = Object.keys(variants);
