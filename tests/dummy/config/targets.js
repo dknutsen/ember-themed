bb596d10a9236e4770e3c8f674718f9d05ad220b
Dan Knutsen <dknutsen@gmail.com>
2020-12-17 20:10:02 -0600

    Remove IE11 from dummy app targets #4

--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -6,12 +6,13 @@ const browsers = [
   'last 1 Safari versions'
 ];
 
+/* dummy app doesn't need to support IE11
 const isCI = Boolean(process.env.CI);
 const isProduction = process.env.EMBER_ENV === 'production';
-
 if (isCI || isProduction) {
   browsers.push('ie 11');
 }
+*/
 
 module.exports = {
   browsers
